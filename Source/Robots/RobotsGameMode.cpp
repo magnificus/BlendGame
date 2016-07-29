6d780e134984d37fc3be5bf16322445e7dadb8c9
unknown <tobiaselinder@gmail.com>
2016-07-29 12:07:29 +0200

    init

---
+++ Source/Robots/RobotsGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "Robots.h"
+#include "RobotsGameMode.h"
+#include "RobotsHUD.h"
+#include "RobotsCharacter.h"
+
+ARobotsGameMode::ARobotsGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = ARobotsHUD::StaticClass();
+}
