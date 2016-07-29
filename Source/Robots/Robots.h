99fe3ee4fa3c9ed2b950f0f6831d0f4a6b6ae0da
unknown <tobiaselinder@gmail.com>
2016-07-29 12:07:29 +0200

    init

---
+++ Source/Robots/Robots.h
@@ -0,0 +1,9 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#ifndef __ROBOTS_H__
+#define __ROBOTS_H__
+
+#include "EngineMinimal.h"
+
+
+#endif
---
+++ Source/Robots/RobotsGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameMode.h"
+#include "RobotsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ARobotsGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	ARobotsGameMode();
+};
+
+
+
