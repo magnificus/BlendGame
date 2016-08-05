fe1ed7e07c104fd5ce7c242360ecdef9fb4b779c
unknown <tobiaselinder@gmail.com>
2016-08-05 14:34:02 +0200

    lagt in animerad robot

---
+++ Source/Robots/RobotsGameState.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameState.h"
+#include "RobotsGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROBOTS_API ARobotsGameState : public AGameState
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
