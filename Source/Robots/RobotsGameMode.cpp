2e8e1995b7fac032a23c7f072519822eef9b7122
unknown <tobiaselinder@gmail.com>
2016-08-06 18:42:23 +0200

    aggresiv ai

--- Source/Robots/RobotsGameMode.cpp
+++ Source/Robots/RobotsGameMode.cpp
@@ -9,7 +9,7 @@ ARobotsGameMode::ARobotsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/RobotChar"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/robotdude/RobotChar"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
